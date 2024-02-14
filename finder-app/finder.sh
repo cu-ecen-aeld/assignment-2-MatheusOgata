a9efc09496c38b244d2d77a649b3dada32722912
MatheusOgata <matheus.ogata@outlook.com>
2024-02-14 15:43:23 -0300

    item 5, 6 and 7 completed.

---
+++ finder-app/finder.sh
@@ -0,0 +1,22 @@
+FILEDIR=$1
+SEARCHSTR=$2
+
+if [ $# -le 1 ] || [ $# -gt 2 ];
+then 
+	echo "ERROR: Invalid number of argument"
+	exit 1
+fi
+
+if [ -d $FILEDIR ]; 
+then
+        cd $FILEDIR
+	echo "The number of files are $(ls -l -R | grep "^-" | wc -l) and the number of matching lines are $(grep -r -n "$SEARCHSTR" | wc -l)" 
+	exit 0
+
+else
+
+	echo "ERROR: $FILEDIR does not represent a directory on the filesystem."
+	exit 1
+
+fi
+
